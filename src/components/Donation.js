2f1dfbd8f96ffc26bb930c7397ce3dfade37c467
Meher Slimani <meher.slimeni@gmail.com>
2021-07-07 16:26:51 +0100

    Add Donation component

--- src/components/Donation.js
+++ src/components/Donation.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../styles/Donation.css'
 
 const Donation = () => {
   return (
-    <div>
-      Donations
+    <div className="donation__container">
+      <p className="donation__text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus nulla quaerat corporis, suscipit sint porro eos necessitatibus adipisci nostrum distinctio officiis possimus labore voluptates! Delectus laborum similique magni qui nam.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      </p>
+      <button className="donation__button">Donate</button>
     </div>
   )
 }
